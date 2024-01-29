0ccccd4706c53c5ff6cbcbd03378bdeb1d081936
Byxis <byxisgeek@gmail.com>
2024-01-29 15:18:20 +0100

    TD1

--- src/TP1/Main.java
+++ src/TP1/Main.java
@@ -17,6 +17,8 @@ public class Main {
         System.out.println("---");
         System.out.println("");
 
+        Tortue t1 = new Tortue(12, EspeceEnum.Caroline, true, FoodEnum.fruits, GenreEnum.male);
+
         System.out.println("En été à Montpellier : 35 degrés celsius valent "+
                 celsius2Farenheit(35)+" en degrés Farenheit");
         System.out.println("L'eau bout à 100 degrés celsius ce qui vaut "+
